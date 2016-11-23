7c4f826982f2556e58b064e992cf555141e5305a
PaolaOlvera27 <apao.iq1@gmail.com>
2016-11-23 13:32:59 -0600

    avance

---
+++ js/main.js
@@ -0,0 +1,55 @@
+$(document).ready(function() {
+    $('select').material_select();
+     Materialize.updateTextFields();
+  });
+
+     var sede = document.getElementById('dropdown1');
+     function crearArreglo(){
+       var mex= [
+         {name:"Ale Bb",src:"img/Alebb.jpg"}
+         {name:"Moni",src:"img/moni.jpg"}
+         {name:"San", src:"img/san.jpg"}
+         {name:"Flor", src:"img/flor.jpg"}
+         {name:"Pao", src:"img/pao.jpg"}
+       ];
+       var alumna= mex[];
+     }
+     function proceso() {
+
+     }
+
+     function mathRamdom(low, high){
+       return low + math.floor(math.Ramdom)*(high-low))}
+       function selectionCountry (){
+         if (select.value== "mex")
+            function proceso(mex);
+            }
+       }
+     }
+
+});
+
+
+function crearArreglo(){
+  var mex= [
+    {name:"Ale Bb",src:"img/Alebb.jpg"}
+    {name:"Moni",src:"img/moni.jpg"}
+    {name:"San", src:"img/san.jpg"}
+    {name:"Flor", src:"img/flor.jpg"}
+    {name:"Pao", src:"img/pao.jpg"}
+  ];
+  var alumna= mex[];
+}
+var foto = document.getElementById('fotoalumna');
+foto.setAtribute("src","alumna.scr");
+function proceso() {
+
+}
+  function selectionCountry (){
+    if (select.value== "mex")
+       function proceso(mex);
+       }
+  }
+}
+
+});
